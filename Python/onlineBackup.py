a2abb7ef9bc7dab1cfd67451622f208041b1ddfd
NikunjRathi1906 <83570245+NikunjRathi1906@users.noreply.github.com>
2021-06-14 18:50:37 +0530

    Add files via upload

---
+++ Python/onlineBackup.py
@@ -0,0 +1,30 @@
+import dropbox
+
+
+class uploadFiles:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, "rb") as f:
+            dbx.files_upload(f.read(), file_to)
+
+
+def main():
+    access_token = "sl.Ayu7glyXm8W1l0SE4wwwoYV1OQ7bP0Momv0H8z5zqpv0LIcg6oeWivJIerAzpSUZGjWcQEs9pRWIvjCmCksiZD8ieuZCKzUlSXH2cbzpamqO8w9C7SX-9v3ro_flNfMpHm_x1DtX1LTJ"
+    uploadF = uploadFiles(access_token)
+
+    file_from = input(
+        "Enter the full path of file including the file name you want to backup: "
+    )
+    file_path = file_from.split("/")
+    file_name = file_path[len(file_path) - 1]
+    file_to = "/" + file_name
+
+    uploadF.upload(file_from, file_to)
+    print("Backup created successfully for " + file_name)
+
+
+main()
