a7ebc1f1bd86d0a2bd84aca4ff8c9c4d98d201d5
NikunjRathi1906 <83570245+NikunjRathi1906@users.noreply.github.com>
2021-07-01 18:53:42 +0530

    Update PRO-C108.py

--- Python/PRO-C108.py
+++ Python/PRO-C108.py
@@ -1,30 +1,61 @@
-"""
-import random
-
-sum = []
-
-for i in range(1000):
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-    sum.append(dice1+dice2)
-
-print(sum)
-
-import plotly.figure_factory as ff
-
-fig = ff.create_distplot([sum], ["Results"], show_hist= False)
-fig.show()
-"""
 import pandas as pd
 import plotly.figure_factory as ff
+import statistics
 
 df = pd.read_csv("Data\PRO-C108.csv")
 
 weight = df["Weight(Pounds)"].tolist()
 height = df["Height(Inches)"].tolist()
 
-heightgraph = ff.create_distplot([height], ["Height"], show_hist=False)
-heightgraph.show()
+mean = statistics.mean(weight)
+median = statistics.median(weight)
+mode = statistics.mode(weight)
+
+print(mean)
+print(median)
+print(mode)
+
+stddev = statistics.stdev(weight)
+print(stddev)
+
+# 1st stddev
+lower = mean - stddev
+upper = mean + stddev
+
+weight1 = []
+for w in weight:
+    if w >= lower and w <= upper:
+        weight1.append(w)
+
+# percentage of data lying in 1st stddev
+percentage1 = (len(weight1) / len(weight)) * 100
+print(percentage1)
+
+# 2nd stddev
+lower2 = mean - (2 * stddev)
+upper2 = mean + (2 * stddev)
+
+weight2 = []
+for w in weight:
+    if w >= lower2 and w <= upper2:
+        weight2.append(w)
+
+# percentage of data lying in 2st stddev
+percentage2 = (len(weight2) / len(weight)) * 100
+print(percentage2)
+
+# 3rd stddev
+lower3 = mean - (3 * stddev)
+upper3 = mean + (3 * stddev)
+
+weight3 = []
+for w in weight:
+    if w >= lower3 and w <= upper3:
+        weight3.append(w)
+
+# percentage of data lying in 3rd stddev
+percentage3 = (len(weight3) / len(weight)) * 100
+print(percentage3)
 
 weightgraph = ff.create_distplot([weight], ["Weight"], show_hist=False)
 weightgraph.show()
